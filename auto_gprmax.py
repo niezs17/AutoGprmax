2b80741698ca7f19746fda570aed9802d231ae99
Buzz2Z <1574610489@qq.com>
2024-02-01 11:17:38 +0800

    完成随机生成充气空洞的任务，water的介电常数过大，图像生成有问题

--- auto_gprmax.py
+++ auto_gprmax.py
@@ -1,13 +1,29 @@
 import os
+import numpy as np
 from gprMax.gprMax import api
 from tools.outputfiles_merge import get_output_data, merge_files
 from tools.plot_Bscan import normalized_mpl_plot
+import random
 
 
-def generate_bscan(text_in_ngeo, text_in_geo, generate, geometry, ascan_times, figure_number, time_window):
+def check_distance(center1, center2, radium):
+    return np.linalg.norm(np.array(center1) - np.array(center2)) < np.sum(radium)
+
+
+def generate_cavity_center(region):
+    x_center = random.uniform(region['x'][0], region['x'][1])
+    y_center = random.uniform(region['y'][0], region['y'][1])
+    return x_center, y_center
+
+
+def create_cavity_instruction(x, y, z1, z2, radium, cavity_type):
+    return f"#cylinder: {round(x,2)} {round(y,2)} {z1} " \
+           f"{round(x,2)} {round(y,2)} {z2} {round(radium,2)} {cavity_type}\n"
+
+
+def generate_bscan(text_in, generate, geometry, ascan_times, figure_number, time_window):
     """
-    :param text_in_ngeo:    不带几何建模指令的输入代码(str)
-    :param text_in_geo:     带有几何建模指令的输入代码(str)
+    :param text_in:    不带几何建模指令的输入代码(str)
     :param generate:        是否重建Bscan
     :param geometry:        是否生成几何建模
                 GEN = 1, GEO = 0      重新合成BScan, 生成图像, 不生成几何建模
@@ -27,11 +43,12 @@ def generate_bscan(text_in_ngeo, text_in_geo, generate, geometry, ascan_times, f
         f = open(in_file_name, 'w')
         if geometry:
             # 从头写入不带几何设置命令的in文件, 只执行1次，因为目的仅为得到建模图像，此时不需要merge
-            f.writelines(text_in_geo)
+            f.writelines(text_in)
+            f.writelines("#geometry_view: 0 0 0 2.00 2.00 0.0025 0.01 0.01 0.0025 basic n \n")
             f.close()
             api(fil_in, n=1, geometry_only=False)
         else:
-            f.writelines(text_in_ngeo)
+            f.writelines(text_in)
             f.close()
             api(fil_in, n=ascan_times, geometry_only=False)  # A-scan 60 times
             # 用模拟出的A-Scan条数合并生成B-Scan图像(注意，这里的文件路径只能从AScan停止， 不能带数字，不能带后缀)
@@ -51,8 +68,56 @@ def generate_bscan(text_in_ngeo, text_in_geo, generate, geometry, ascan_times, f
         # 提取输出数据
         out_file_name = r"./text_in/figure" + str(figure_number) + "/Ascan_merged.out"  # 合并后文件名
         outputdata, dt = get_output_data(out_file_name, 1, 'Ez')
-        plt = normalized_mpl_plot(out_file_name, outputdata, dt * 1e9, 1, 'Ez', ascan_times, time_window)
+        plt = normalized_mpl_plot(out_file_name, outputdata, dt * 1e9, 1, 'Ex', ascan_times, time_window)
         plt.savefig(r"./scan_out" + '/Bscan' + str(figure_number) + '.jpg')
         print("saving succeed")
         print("looking *.out/*.in files in \'figure_x\'")
         print("looking *.jpg files in \'scan_out\'")
+
+
+def random_cavity(soil_base_space, air_cavity_num_all, water_cavity_num_all):
+    """
+    :param soil_base_space:     路基空间范围
+    :param air_cavity_num_all:      充气空洞数量
+    :param water_cavity_num_all:    充水空洞数量
+    :return: 完整in文件指令
+    """
+    new_inst = []
+    if air_cavity_num_all + water_cavity_num_all > 3:
+        cavity_x = (soil_base_space['x1'] + soil_base_space['x2']) / 2.0
+        cavity_y = (soil_base_space['y1'] + soil_base_space['y2']) / 2.0
+        cavity_radium = 0.2
+        cavity_type = 'free_space'
+        new_inst.append(create_cavity_instruction(cavity_x, cavity_y, soil_base_space['z1'], soil_base_space['z2'],
+                                                  cavity_radium, cavity_type))
+        print("Too many cavities!\n "
+              "Automatically generate an cylinder air cavity with the center of soil base, r = 0.2")
+        return ''.join(new_inst)
+    # 随机生成空洞大小，先处理充气空洞
+    else:
+        cavity_radium = []
+        cavity_centers = []
+        for _ in range(air_cavity_num_all + water_cavity_num_all):
+            cavity_radium.append(random.uniform(0.02, 0.20))        # 随机生成空洞半径
+            # print(cavity_radium[-1])
+            cavity_region = {'x': [soil_base_space['x1'] + np.max(cavity_radium),
+                                   soil_base_space['x2'] - np.max(cavity_radium)],
+                             'y': [soil_base_space['y1'] + np.max(cavity_radium),
+                                   soil_base_space['y2'] - np.max(cavity_radium)]}
+            x, y = generate_cavity_center(cavity_region)
+            cavity_centers.append((x, y))
+            # Check distance from previous cavities
+            if all(not check_distance((x, y), center, cavity_radium[-2:]) for center in cavity_centers[:-1]):
+                cavity_type = 'free_space' if _ < air_cavity_num_all else 'water'
+                new_inst.append(create_cavity_instruction(x, y, soil_base_space['z1'], soil_base_space['z2'],
+                                                          cavity_radium[-1], cavity_type))
+            else:
+                # Adjusting cavity radius or position if necessary
+                # Or handle overlapping cavities
+                pass
+        return ''.join(new_inst)
+
+
+# print(random_cavity({'x1': 0, 'y1': 0, 'z1': 0, 'x2': 2.0, 'y2': 1.6, 'z2': 0.0025}, 3, 0))
+
+
--- main.py
+++ main.py
@@ -1,37 +1,39 @@
-import auto_gprmax
+from auto_gprmax import random_cavity
+from auto_gprmax import generate_bscan
 
-# 初始化gprMax模拟的基础配置(不变量)
-t1 = "#domain: 2.5 0.5 0.002\n"  # 模型尺寸 #domain: f1 f2 f3 (m)
-t2 = "#dx_dy_dz: 0.005 0.005 0.002\n"  # 空间步长 #dx_dy_dz: f1 f2 f3
-t3 = "#time_window: 15e-9\n"  # 总模拟时间
-t4 = "#waveform: ricker 1.0 900e6 my_ricker\n"  # 源波形
-t5 = "#hertzian_dipole: z 0.0875 0.4525 0 my_ricker\n"  # 赫兹偶极子:模拟理想化的天线源
-t6 = "#rx: 0.1125 0.4525 0\n"  # 接收点 (x, y, z)
-t7 = "#src_steps: 0.02 0.0 0\n"  # 源点移动步长
-t8 = "#rx_steps: 0.02 0.0 0\n"  # 接收点移动步长
-t17 = "#geometry_view: 0 0 0 2.5 0.5 0.0025 0.005 0.005 0.0025 model n\n"  # 几何视图设置
+# 无缺损的道路模型
+TEXT_INTACT_ROAD \
+    = ("#domain: 2.00 2.00 0.0025\n"
+       "#dx_dy_dz: 0.01 0.01 0.0025\n"
+       "#time_window: 16e-9\n"
+       "#waveform: ricker 1.0 400e6 my_ricker\n"
+       "#hertzian_dipole: z 0.2 1.905 0 my_ricker\n"
+       "#rx: 0.3 1.905 0\n"
+       "#src_steps: 0.02 0.0 0\n"
+       "#rx_steps: 0.02 0.0 0\n"
+       "#material: 4 0.005 1 0 asphalt\n"
+       "#material: 9 0.05 1 0 concrete\n"
+       "#material: 12 0.1 1 0 soilbase\n"
+       "#box: 0.00 0.00 0.00 2.00 1.60 0.0025 soilbase\n"
+       "#box: 0.00 1.60 0.00 2.00 1.75 0.0025 concrete\n"
+       "#box: 0.00 1.75 0.00 2.00 1.90 0.0025 asphalt\n"
+       "#box: 0.00 1.90 0.00 2.00 2.00 0.0025 free_space\n"
+       )
 
-# 初始化gprMax模拟的基础配置(变量)
-t9 = "#material: 6.4 0.01 1 0 concrete\n"  #
-t10 = "#box: 0 0 0 2.5 0.45 0.0025 concrete\n"  #
-t11 = "#box: 0 0.45 0 2.5 0.5 0.0025 free_space\n"  #
-t12 = "#cylinder: 0.65 0.10 0.00 0.65 0.10 0.002 0.0119 pec\n"  #
-t13 = "#cylinder: 1.01 0.15 0.00 1.01 0.15 0.002 0.0070 pec\n"  #
-t14 = "#cylinder: 0.75 0.20 0.00 0.75 0.20 0.002 0.0070 pec\n"  #
-t15 = "#cylinder: 1.44 0.31 0.00 1.44 0.31 0.002 0.0119 pec\n"  #
-t16 = "#cylinder: 1.43 0.11 0.00 1.43 0.11 0.002 0.0119 pec\n"  #
+# 定义要生成的文件数量generate
+generate_num = 1
+# 路基空间范围(x1, x2, y1, y2, z1, z2) y2作为y方向的起始坐标
+soil_base_space = {'x1': 0, 'y1': 0, 'z1': 0, 'x2': 2.0, 'y2': 1.6, 'z2': 0.0025}
+# 加上缺损
+TEXT_IN = TEXT_INTACT_ROAD + random_cavity(soil_base_space, 3, 0)
 
-generate_num = 1  # 定义要生成的文件数量generate
-TEXT_NGEO = t1 + t2 + t3 + t4 + t5 + t6 + t7 + t8 + t9 + t10 + t11 + t12 + t13 + t14 + t15 + t16
-TEXT_GEO = TEXT_NGEO + t17
-
+# 生成Bscan图像
 for figure in range(1, generate_num + 1, 1):
-    auto_gprmax.generate_bscan(
-        text_in_ngeo=TEXT_NGEO,
-        text_in_geo=TEXT_GEO,
+    generate_bscan(
+        text_in=TEXT_IN,
         generate=1,
         geometry=0,
-        ascan_times=60,
+        ascan_times=70,
         figure_number=figure,
-        time_window=15e-9
+        time_window=16e-9
     )
