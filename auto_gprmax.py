d0b150b6c2689107ac52493bef58a85c531200fc
Buzz2Z <1574610489@qq.com>
2024-02-03 23:47:52 +0800

    封装了generate函数，简化主函数，找到了生成空洞的最低深度，找到了正确的domain, dx_dy_dz, waveform, hertzian_dipole, rx, src_steps, rx_steps, material, box

--- auto_gprmax.py
+++ auto_gprmax.py
@@ -4,6 +4,8 @@ from gprMax.gprMax import api
 from tools.outputfiles_merge import get_output_data, merge_files
 from tools.plot_Bscan import normalized_mpl_plot
 import random
+
+
 # import cv2
 
 
@@ -16,39 +18,43 @@ def create_cavity_instruction(x, y, z1, z2, r, cavity_type):
     shape = ['box', 'cylinder']
     random_shape = random.choice(shape)
     if random_shape == 'box':
-        return f"#{random_shape}: {round(x-r,2)} {round(y-r,2)} {z1} " \
-                f"{round(x+r,2)} {round(y+r,2)} {z2} {cavity_type}\n"
+        return f"#{random_shape}: {round(x - r, 2)} {round(y - r, 2)} {z1} " \
+               f"{round(x + r, 2)} {round(y + r, 2)} {z2} {cavity_type}\n"
     else:
-        return f"#{random_shape}: {round(x,2)} {round(y,2)} {z1} " \
-                f"{round(x,2)} {round(y,2)} {z2} {round(r,2)} {cavity_type}\n"
+        return f"#{random_shape}: {round(x, 2)} {round(y, 2)} {z1} " \
+               f"{round(x, 2)} {round(y, 2)} {z2} {round(r, 2)} {cavity_type}\n"
 
 
 def generate_bscan(text_in, regenerate, geometry, ascan_times, figure_number, time_window, info):
     """
-    :param text_in:    不带几何建模指令的输入代码(str)
-    :param regenerate:        是否重建Bscan
+    :param text_in:         不带几何建模指令的输入代码(str)
+    :param regenerate:      是否重建Bscan
     :param geometry:        是否生成几何建模
-                GEN = 1, GEO = 0      重新合成BScan, 生成图像, 不生成几何建模
-                GEN = 0, GEO = 0      不合成Bscan, 生成图像, 不生成几何建模
-                GEN = 0, GEO = 1      无事发生
-                GEN = 1, GEO = 1      不生成新的Bscan数据和图像, 只生成几何建模
-    :param ascan_times:      Ascan扫描次数
+    :param ascan_times:     Ascan扫描次数
     :param figure_number:   生成图像的编号
     :param time_window:     仿真时间
     :param info:            生成图像基本信息 格式: air_x_water_y(x表示充气空洞数量，y表示充水空洞数量)
     :return None
     """
     figure_path = f"./text_in/basic_{info}_{figure_number}"
-    in_file_name = f"{figure_path}/Ascan.in"
     text_geo = "#geometry_view: 0 0 0 2.00 2.00 0.0025 0.0025 0.0025 0.0025 basic n \n"
 
-    # 创建目录
-    if not os.path.exists(figure_path):
+    while os.path.exists(figure_path):
+        figure_number += 1
+        figure_path = f"./text_in/basic_{info}_{figure_number}"
+
+    if (regenerate and not geometry) or not regenerate:
+        figure_number -= 1
+        figure_path = f"./text_in/basic_{info}_{figure_number}"
+    else:
+        figure_path = f"./text_in/basic_{info}_{figure_number}"
         os.makedirs(figure_path)
+    in_file_name = f"{figure_path}/Ascan.in"
 
     # 处理生成逻辑
     if regenerate:
         if geometry:
+            # 创建目录  在存文件之前，检查先前是否已经生成过同名文件，若有，则存在该文件编号后面
             with open(in_file_name, 'w') as f:
                 # 处理几何建模 在原有文本基础上加一行
                 f.write(text_in)
@@ -70,7 +76,7 @@ def generate_bscan(text_in, regenerate, geometry, ascan_times, figure_number, ti
         out_file_name = f"{figure_path}/Ascan_merged.out"
         outputdata, dt = get_output_data(out_file_name, 1, 'Ez')
         plt = normalized_mpl_plot(out_file_name, outputdata, dt * 1e9, 1, 'Ex', ascan_times, time_window)
-        plt.savefig(f"./scan_out/bscan_{info}_{figure_number}.jpg")
+        plt.savefig(f"{figure_path}/bscan_{info}_{figure_number}.jpg")
         print(f"{info}_{figure_number} saving succeeded.")
         print("looking *.out/*.in files in \'figure_x\'")
         print("looking *.jpg files in \'scan_out\'")
@@ -90,7 +96,7 @@ def random_cavity(soil_base_space, air_cavity_num_all, water_cavity_num_all):
     soil_base_space['x2'] -= 0.1
     soil_base_space['y2'] -= 0.1
     # 如果生成数量过多，则默认只生成一个充气型空洞，位于路基正中间
-    if air_cavity_num_all + water_cavity_num_all > 3:
+    if air_cavity_num_all + water_cavity_num_all > 5:
         cavity_x = (soil_base_space['x1'] + soil_base_space['x2']) / 2.0
         cavity_y = (soil_base_space['y1'] + soil_base_space['y2']) / 2.0
         cavity_radium = 0.2
@@ -105,7 +111,7 @@ def random_cavity(soil_base_space, air_cavity_num_all, water_cavity_num_all):
         cavity_centers = []
         cavity_num = 0
         while cavity_num < (air_cavity_num_all + water_cavity_num_all):
-            cavity_radium.append(random.uniform(0.02, 0.20))        # 随机生成空洞半径
+            cavity_radium.append(random.uniform(0.05, 0.20))  # 随机生成空洞半径
             # print(cavity_radium[-1])
             cavity_region = {'x': [soil_base_space['x1'] + np.max(cavity_radium),
                                    soil_base_space['x2'] - np.max(cavity_radium)],
@@ -127,9 +133,56 @@ def random_cavity(soil_base_space, air_cavity_num_all, water_cavity_num_all):
         return ''.join(new_inst)
 
 
+def generate(TEXT_INTACT_ROAD, generate_num, air_cavity_num,
+             water_cavity_num, soil_base_space, time_window, generate_mode):
+    for _ in range(1, generate_num + 1, 1):
+        TEXT_IN = TEXT_INTACT_ROAD + random_cavity(soil_base_space, air_cavity_num, water_cavity_num)
+        # TEXT_IN = TEXT_INTACT_ROAD
+        # 生成Bscan图像
+        if generate_mode == 'geo':
+            generate_bscan(
+                TEXT_IN,
+                regenerate=1,
+                geometry=1,
+                ascan_times=10,
+                figure_number=_,
+                time_window=time_window,
+                info=f"air_{air_cavity_num}_water_{water_cavity_num}"
+            )
+        elif generate_mode == 'all':
+            generate_bscan(
+                TEXT_IN,
+                regenerate=1,
+                geometry=1,
+                ascan_times=10,
+                figure_number=_,
+                time_window=time_window,
+                info=f"air_{air_cavity_num}_water_{water_cavity_num}"
+            )
+            generate_bscan(
+                TEXT_IN,
+                regenerate=1,
+                geometry=0,
+                ascan_times=10,
+                figure_number=_,
+                time_window=time_window,
+                info=f"air_{air_cavity_num}_water_{water_cavity_num}"
+            )
+        elif generate_mode == 'plot':
+            generate_bscan(
+                TEXT_IN,
+                regenerate=0,
+                geometry=0,
+                ascan_times=10,
+                figure_number=_,
+                time_window=time_window,
+                info=f"air_{air_cavity_num}_water_{water_cavity_num}"
+            )
+        else:
+            print("generate_mode error")
+
+
 if __name__ == '__main__':
     for _ in range(0, 10):
-        print(random_cavity({'x1': 0, 'y1': 0, 'z1': 0, 'x2': 2.0, 'y2': 1.6, 'z2': 0.0025}, 2, 1))
+        print(random_cavity({'x1': 0, 'y1': 0.6, 'z1': 0, 'x2': 2.0, 'y2': 1.6, 'z2': 0.0025}, 2, 1))
         print()
-
-
--- main.py
+++ main.py
@@ -1,16 +1,17 @@
-from auto_gprmax import random_cavity
-from auto_gprmax import generate_bscan
+from auto_gprmax import generate
+
+time_window = 50e-9
 
 # 无缺损的道路模型
 TEXT_BASE \
     = ("#domain: 2.00 2.00 0.0025\n"
        "#dx_dy_dz: 0.0025 0.0025 0.0025\n"
-       "#time_window: 32e-9\n"
+       f"#time_window: {time_window}\n"
        "#waveform: ricker 1.0 600e6 my_ricker\n"
        "#hertzian_dipole: z 0.0875 1.905 0 my_ricker\n"
        "#rx: 0.1125 1.905 0\n"
-       "#src_steps: 0.031 0.0 0\n"
-       "#rx_steps: 0.031 0.0 0\n"  
+       "#src_steps: 0.18 0.0 0\n"
+       "#rx_steps: 0.18 0.0 0\n"  
        "#material: 4 0.005 1 0 asphalt\n"   
        "#material: 9 0.05 1 0 concrete\n"
        "#material: 12 0.1 1 0 soilbase\n"
@@ -19,46 +20,9 @@ TEXT_BASE \
        "#box: 0.00 1.60 0.00 2.00 1.75 0.0025 concrete\n"
        "#box: 0.00 1.75 0.00 2.00 1.90 0.0025 asphalt\n"
        "#box: 0.00 1.90 0.00 2.00 2.00 0.0025 free_space\n"
-       # "#cylinder: 0.60 1.26 0 0.60 1.26 0.0025 0.1 free_space\n"
-       # "#cylinder: 1.40 1.26 0 1.40 1.26 0.0025 0.1 water\n"
        )
 
 
-def generate(TEXT_INTACT_ROAD, generate_num, air_cavity_num, water_cavity_num, soil_base_space, regenerate, geometry):
-    for _ in range(1, generate_num + 1, 1):
-        TEXT_IN = TEXT_INTACT_ROAD + random_cavity(soil_base_space, air_cavity_num, water_cavity_num)
-        # TEXT_IN = TEXT_INTACT_ROAD
-        # 生成Bscan图像
-        generate_bscan(
-            TEXT_IN,
-            regenerate,
-            geometry,
-            ascan_times=60,
-            figure_number=_,
-            time_window=64e-9,
-            info=f"air_{air_cavity_num}_water_{water_cavity_num}"
-        )
-
-
-_regenerate_ = 1
-_geometry_ = 0
-
 if __name__ == '__main__':
-    generate(TEXT_INTACT_ROAD=TEXT_BASE, generate_num=2, air_cavity_num=3, water_cavity_num=0,
-             soil_base_space={'x1': 0, 'y1': 0, 'z1': 0, 'x2': 2.0, 'y2': 1.6, 'z2': 0.0025},
-             regenerate=_regenerate_, geometry=_geometry_)
-    generate(TEXT_INTACT_ROAD=TEXT_BASE, generate_num=2, air_cavity_num=2, water_cavity_num=1,
-             soil_base_space={'x1': 0, 'y1': 0, 'z1': 0, 'x2': 2.0, 'y2': 1.6, 'z2': 0.0025},
-             regenerate=_regenerate_, geometry=_geometry_)
-    generate(TEXT_INTACT_ROAD=TEXT_BASE, generate_num=2, air_cavity_num=1, water_cavity_num=2,
-             soil_base_space={'x1': 0, 'y1': 0, 'z1': 0, 'x2': 2.0, 'y2': 1.6, 'z2': 0.0025},
-             regenerate=_regenerate_, geometry=_geometry_)
-    generate(TEXT_INTACT_ROAD=TEXT_BASE, generate_num=2, air_cavity_num=2, water_cavity_num=0,
-             soil_base_space={'x1': 0, 'y1': 0, 'z1': 0, 'x2': 2.0, 'y2': 1.6, 'z2': 0.0025},
-             regenerate=_regenerate_, geometry=_geometry_)
-
-
-# if __name__ == '__main__':
-#     generate(TEXT_INTACT_ROAD=TEXT_BASE, generate_num=1, air_cavity_num=1, water_cavity_num=1,
-#              soil_base_space={'x1': 0, 'y1': 0, 'z1': 0, 'x2': 2.0, 'y2': 1.6, 'z2': 0.0025},
-#              regenerate=_regenerate_, geometry=_geometry_)
+    generate(TEXT_INTACT_ROAD=TEXT_BASE, generate_num=1, air_cavity_num=0, water_cavity_num=1, time_window=time_window,
+             soil_base_space={'x1': 0, 'y1': 0.8, 'z1': 0, 'x2': 2.0, 'y2': 1, 'z2': 0.0025}, generate_mode='all')
