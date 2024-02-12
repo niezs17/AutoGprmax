c546224f1e9c8dddbafc633dd9a8bd02ed2a0fe6
Buzz2Z <1574610489@qq.com>
2024-02-12 22:22:34 +0800

    优化 file.py 1. 避免result目录中的文件被视为待复制的原文件 2. 避免在way == 'content'时重复执行字符串拼接和目录创建的操作 3. 增加remove_all_jpgs(way)安全删除图像文件

--- file.py
+++ file.py
@@ -39,6 +39,7 @@ def classify_all_jpgs(way):
     new_file = []
     overwrite_file = []
     count = {'air1_water2': 0, 'air2_water1': 0, 'air0_water3': 0, 'air3_water0': 0}
+
     if way == 'date':
         print("\nSave by date...\n")
     elif way == 'content':
@@ -46,46 +47,93 @@ def classify_all_jpgs(way):
     else:
         print("\nargs error: \'way\'\n")
         return
+
     for root, dirs, files in os.walk('B-scan'):
+        # 跳过result目录中的文件
+        if 'result' in root:
+            continue
+
         for file in files:
-            # 检查文件是否是.jpg文件
             if file.lower().endswith('.jpg'):
-                # 构建源文件完整路径
                 file_path = os.path.join(root, file)
+                parent_dir = os.path.abspath(os.path.join(root, os.pardir))
+
                 if way == 'date':
-                    # 查找所有子目录下的.jpg文件，并将其存入每个日期对应文件夹内(位于每张查找到的图像的上一级中result文件夹)
-                    # 获取图片的上一级目录路径
-                    parent_dir = os.path.abspath(os.path.join(root, os.pardir))
-                    # 确保上一级目录中存在名为result的文件夹
                     result_dir = os.path.join(parent_dir, 'result')
-                    if not os.path.exists(result_dir):
-                        os.makedirs(result_dir)
-                    # 构建目标文件完整路径
-                    dest_file_path = os.path.join(result_dir, file)
                 elif way == 'content':
-                    cavity_class = f"air{''.join(file)[10]}_water{''.join(file)[18]}"
+                    cavity_class = f"air{file[10]}_water{file[18]}"
                     count[cavity_class] += 1
                     result_dir = os.path.join('figure', cavity_class)
-                    if not os.path.exists(result_dir):
-                        os.makedirs(result_dir)
-                    # 构建目标文件完整路径
-                    dest_file_path = os.path.join(result_dir, f"{count[cavity_class]}.jpg")
                 else:
                     break
-                # 复制文件
+
+                if not os.path.exists(result_dir):
+                    os.makedirs(result_dir)
+
+                if way == 'date':
+                    dest_file_path = os.path.join(result_dir, file)
+                elif way == 'content':
+                    dest_file_path = os.path.join(result_dir, f"{count[cavity_class]}.jpg")
+
                 if os.path.exists(dest_file_path):
                     overwrite_file.append(f"overwrite: {file_path} -> {dest_file_path}\n")
                 else:
                     new_file.append(f"add: {file_path} -> {dest_file_path}\n")
                 shutil.copy(file_path, dest_file_path)
-    if len(new_file) > 0:
-        print(f"\n** New   \ncount: {len(new_file):>5}\n{''.join(new_file)}")
+
+    if new_file:
+        print(f"\n** New   \n{''.join(new_file)}\ncount: {len(new_file):>5}\n")
     else:
-        print(f"No files added")
-    if len(overwrite_file) > 0:
-        print(f"\n** Overwrite   \ncount: {len(overwrite_file):>5}\n{''.join(overwrite_file)}")
+        print("No files added")
+
+    if overwrite_file:
+        print(f"\n** Overwrite   \n{''.join(overwrite_file)}\ncount: {len(overwrite_file):>5}\n")
     else:
-        print(f"No files overwrote")
+        print("No files overwrote")
+
+
+def remove_all_jpgs(way):
+    """
+    安全删除图像
+    :way='date':     删除时间分类文件夹
+    :way='content':  删除内容分类文件夹
+    :return: None
+    """
+    # 遍历root_dir下的所有文件和文件夹
+    remove_file = []
+    if way == 'date':
+        print("\nRomove by date...\n")
+        for root, dirs, files in os.walk('B-scan'):
+            # 检查当前目录名是否为result
+            if os.path.basename(root) == 'result':
+                # 遍历当前result文件夹下的所有文件
+                for file in files:
+                    # 假设图片文件的扩展名为.jpg等
+                    if file.lower().endswith('.jpg'):
+                        # 构建图片的完整路径
+                        file_path = os.path.join(root, file)
+                        remove_file.append(f"remove: {file_path}\n")
+                        # 删除图片
+                        os.remove(file_path)
+    elif way == 'content':
+        print("\nRemove by content...\n")
+        for root, dirs, files in os.walk('figure'):
+            # 遍历当前figure文件夹下的所有文件
+            for file in files:
+                # 假设图片文件的扩展名为.jpg等
+                if file.lower().endswith('.jpg'):
+                    # 构建图片的完整路径
+                    file_path = os.path.join(root, file)
+                    remove_file.append(f"remove: {file_path}\n")
+                    # 删除图片
+                    os.remove(file_path)
+    else:
+        print("args error: \'way\'")
+        return
+    if len(remove_file) > 0:
+        print(f"\n** Remove   \n{''.join(remove_file)}\ncount: {len(remove_file):>5}\n")
+    else:
+        print(f"No files removed")
 
 
 # 使用示例
@@ -94,6 +142,3 @@ if __name__ == '__main__':
     classify_all_jpgs(way='date')
     classify_all_jpgs(way='content')
     count_all_jpgs()
-
-# 下次更新思路:
-# 1.
