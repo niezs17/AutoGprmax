ef0ae56cb03390b7ea5f6c57d068c14f8d5396d8
Buzz2Z <1574610489@qq.com>
2024-02-14 21:36:05 +0800

    优化 run.bat 1. 可输入参数控制生成图像的种类和数量 2. 增加预生成图片信息提示功能，用户可以再次确认是否继续生成

--- run.bat
+++ run.bat
@@ -1,14 +1,42 @@
 @echo off
+
+cls
 setlocal
+
+set CONDA_PATH=C:\Users\NIEZS\miniconda3
+set CONDA_ENV=gprMax
 set script_path=E:\PAPPER\PROCESS\GprMax\gpr_in\generate.py
 set /a count=0
+set /a air_num = %1
+set /a water_num = %2
+set /a times=%3
+
+CALL %CONDA_PATH%\Scripts\activate.bat %CONDA_PATH%
+CALL conda activate %CONDA_ENV%
+
+echo **************************************************************
+echo conda path: %CONDA_PATH%
+echo conda env : %CONDA_ENV%
+echo script path: %script_path%
+echo Bscan figure: air num = %air_num% water num = %water_num% times = %times%
+echo **************************************************************
+
+set /p user_input="start to generate? (y/n): "
+if /I "%user_input%"=="y" (
+goto loop
+)
+(
+goto end
+)	
+
 :loop
-if %count% == 115 goto :end
-echo We will generate B-scan figure for %count% times...
-python "%script_path%" 1 2
+if %count% == %times% goto :end
+python "%script_path%" %air_num% %water_num%
 set /a count+=1
 cls
 goto :loop
+
 :end
-echo finish the task, have generated %count% figures...
+echo task end...have generated %count% figures...
 endlocal
+
