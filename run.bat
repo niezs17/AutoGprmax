3f607d271ab19a789316b1d63cbf3dd833ce61df
Buzz2Z <1574610489@qq.com>
2024-07-14 23:20:19 +0800

    需要发布的代码

--- run.bat
+++ run.bat
@@ -3,15 +3,23 @@
 cls
 setlocal
 
-set CONDA_PATH=C:\Users\NIEZS\miniconda3
-set CONDA_ENV=gprMax
-set script_path=E:\PAPPER\PROCESS\GprMax\gpr_in\generate.py
+REM  Here is the part you need to modify !!!
+
+REM Set the path to your CUDA installation
+set CONDA_PATH=[YOUR_CUDA_PATH]
+REM Set the name of your GprMax conda environment
+set CONDA_ENV=[YOUR_GPRMAX_ENVIRONMENT_NAME]
+REM Set the path to the dataset generation script
+set script_path=[./generate.py]
+REM Set the number of air-filled cavities
+set /a air_num=[NUMBER_OF_AIR_FILLED_CAVITIES]
+REM Set the number of water-filled cavities
+set /a water_num=[NUMBER_OF_WATER_FILLED_CAVITIES]
+REM Set the number of scans
+set /a times=[NUMBER_OF_SCANS]
+
+REM  Here is the part you usually no need to modify !!!
 set /a count=0
-set /a air_num=2
-set /a water_num=0
-set /a times=120
-
-
 CALL %CONDA_PATH%\Scripts\activate.bat %CONDA_PATH%
 CALL conda activate %CONDA_ENV%
 
